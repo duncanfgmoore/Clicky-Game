4117303ab6989d5add0d63fa91b42b845280f927
duncanfgmoore <duncanfgmoore@gmail.com>
2018-09-22 13:15:47 -0500

    commit

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -4,7 +4,7 @@ import "./Card.css";
 const Cards = props => (
   <div className="card">
     <div className="img-container">
-      <img alt={props.name} src={props.image} id={props.id} onClick={() => props.imageClicked(props.id)}/>
+      <img alt={props.name} src={process.env.PUBLIC_URL ? process.env.PUBLIC_URL + "/" + props.image : props.image} id={props.id} onClick={() => props.imageClicked(props.id)}/>
     </div>
   </div>
 );
